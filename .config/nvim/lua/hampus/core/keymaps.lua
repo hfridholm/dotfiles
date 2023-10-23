be028dfaef0951fdb3fb419c3febd3ce39b74980
Hampus Fridholm <hampus.fridholm@gmail.com>
2023-10-23 20:18:49 +0200

    Removed lazy-lock.json from tracked files. Added simple neofetch config. Added core keymaps to neovim.

--- .config/nvim/lua/hampus/core/keymaps.lua
+++ .config/nvim/lua/hampus/core/keymaps.lua
@@ -8,7 +8,16 @@ vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
+vim.keymap.set("n", "<leader>sh", "<C-w>h")
+vim.keymap.set("n", "<leader>sj", "<C-w>j")
+vim.keymap.set("n", "<leader>sk", "<C-w>k")
+vim.keymap.set("n", "<leader>sl", "<C-w>l")
+
 vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 vim.keymap.set("n", "<leader>tn", ":tabnext<CR>") -- go to next tab
 vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>") -- go to previous tab
+
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>")
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
