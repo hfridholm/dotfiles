a1a6c983769fbf0dfdb8eb221e7a0d5be2b7c098
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-04-18 09:44:03 +0200

    Added markdown-preview.lua, added IP addresses to neofetch, changed colorscheme.lua, added code to nvim-cmp and added vue to treesitter.

--- .config/nvim/lua/hampus/plugins/treesitter.lua
+++ .config/nvim/lua/hampus/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      ensure_installed = { "vue", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
