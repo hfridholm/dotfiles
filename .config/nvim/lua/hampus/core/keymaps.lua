a0e7f321703436e4d825acaca13600826f3f4ed0
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-11-14 13:45:59 +0100

    Added tmux.conf and vim-tmux-navigator.lua plugin for nvim. Now tmux and nvim is integrated with each other.

--- .config/nvim/lua/hampus/core/keymaps.lua
+++ .config/nvim/lua/hampus/core/keymaps.lua
@@ -2,21 +2,31 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>wv", "<C-w>v")     -- split window vertically
-vim.keymap.set("n", "<leader>wh", "<C-w>s")     -- split window horizontally
-vim.keymap.set("n", "<leader>we", "<C-w>=")     -- make split windows equal width
-vim.keymap.set("n", "<leader>wx", ":close<CR>") -- close window
+-- Windows
+vim.keymap.set("n", "<leader>wv", "<C-w>v")     -- Split window vertically
+vim.keymap.set("n", "<leader>wh", "<C-w>s")     -- Split window horizontally
+vim.keymap.set("n", "<leader>we", "<C-w>=")     -- Make split windows equal width
+vim.keymap.set("n", "<leader>wx", ":close<CR>") -- Close window
 
-vim.keymap.set("n", "<leader>sh", "<C-w>h") -- change to left  window
-vim.keymap.set("n", "<leader>sl", "<C-w>l") -- change to right window
-vim.keymap.set("n", "<leader>sj", "<C-w>j") -- change to under window
-vim.keymap.set("n", "<leader>sk", "<C-w>k") -- change to over  window
+-- Switch between windows
+vim.keymap.set("n", "<leader>sh", "<C-w>h") -- Switch to left  window
+vim.keymap.set("n", "<leader>sl", "<C-w>l") -- Switch to right window
+vim.keymap.set("n", "<leader>sj", "<C-w>j") -- Switch to under window
+vim.keymap.set("n", "<leader>sk", "<C-w>k") -- Switch to over  window
 
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>")      -- new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>")    -- close tab
-vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")     -- tab next
-vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>") -- tab previous
+-- Tabs
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>")
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>")
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")
+vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>")
 
-vim.keymap.set("n", "<leader>bn", ":bnext<CR>")     -- buffer next
-vim.keymap.set("n", "<leader>bp", ":bprevious<CR>") -- buffer previous
-vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")   -- delete buffer
+-- Buffers
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>")
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
+
+-- Navigate between windows better
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>") -- Switch to left  window
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>") -- Switch to right window
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>") -- Switch to under window
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>") -- Switch to over  window
